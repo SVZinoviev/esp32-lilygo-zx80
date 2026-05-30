d75cd3f35e30b90655809939dd2ced02dcbdd282
Sergio <svzinoviev@gmail.com>
2026-05-30 17:22:39 +0200

    Implement video output and keyboard input

--- main/main.c
+++ main/main.c
@@ -5,6 +5,9 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "st7789v_esp_driver.h"
+#include "zx_keyboard.h"
+#include "zx_spectrum_emulator.h"
+#include "zx_video.h"
 
 // TODO: move pins to the board pins file
 #define BOARD_POWERON        15
@@ -22,9 +25,15 @@ void power_turn_on(bool is_on)
 void app_main(void)
 {
     power_turn_on(true);
-    st7789v_esp_driver_init(NULL);
+    zx_video_init();
+    st7789v_esp_driver_init(zx_video_on_lcd_trans_done);
+    zx_keyboard_init();
+    zx_spectrum_init();
 
+    int cycles = 70000;
     while(true) {
-        vTaskDelay(100);
+        zx_spectrum_run(cycles);
+        zx_spectrum_interrupt(0xFF);
+        zx_video_render();
     }
 }
